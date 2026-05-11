255beb1fdb9ec062104df2941b9e149005ebd89a
Berat Belge <beratbelge03@gmail.com>
2026-05-11 22:35:59 +0300

    en_son_hali
    en_son_hali

--- anasayfa.cpp
+++ anasayfa.cpp
@@ -24,27 +24,46 @@ AnaSayfa::~AnaSayfa() {
 }
 
 void AnaSayfa::ilanlariYukle() {
-    ui->listVitrin->clear();
-    ui->listVitrin->setIconSize(QSize(120, 120));
+    ui->listVitrin->clear(); // Listeyi temizle
+
+    // Tasarım Ayarları: İkonların boyutunu ve metnin konumunu ayarla
+    ui->listVitrin->setViewMode(QListView::IconMode); // İkon modu (Resim üstte metin altta)
+    ui->listVitrin->setIconSize(QSize(150, 150));    // Resim boyutu
+    ui->listVitrin->setResizeMode(QListView::Adjust); // Pencere boyutuna göre hizala
+    ui->listVitrin->setSpacing(20);                  // İlanlar arası boşluk
+    ui->listVitrin->setWordWrap(true);               // Uzun başlıkları alt satıra geçir
 
     QSqlDatabase db = DatabaseManager::getInstance()->getDatabase();
     QSqlQuery query(db);
 
+    // Veritabanından Başlık, Fiyat ve Fotoğraf Yolu bilgilerini çekiyoruz
     if (query.exec("SELECT baslik, fiyat, fotografYolu FROM Ilan")) {
         while (query.next()) {
-            QListWidgetItem *item = new QListWidgetItem();
-            item->setText(query.value("baslik").toString() + "\n" + query.value("fiyat").toString() + " TL");
-            item->setTextAlignment(Qt::AlignCenter);
+            QString baslik = query.value("baslik").toString();
+            QString fiyat = query.value("fiyat").toString();
+            QString fotoYolu = query.value("fotografYolu").toString();
 
-            QString foto = query.value("fotografYolu").toString();
-            if (!foto.isEmpty()) {
-                item->setIcon(QIcon(foto));
+            QListWidgetItem *item = new QListWidgetItem();
+
+            // Metni Oluştur: Üstte Başlık, bir alt satırda Fiyat
+            // \n karakteri fiyatı başlığın altına atar
+            item->setText(baslik + "\n" + fiyat + " TL");
+            item->setTextAlignment(Qt::AlignCenter); // Metni ortala
+
+            // Fotoğraf varsa yükle, yoksa varsayılan ikon koy
+            if (!fotoYolu.isEmpty()) {
+                item->setIcon(QIcon(fotoYolu));
             } else {
                 item->setIcon(style()->standardIcon(QStyle::SP_FileIcon));
             }
 
+            // Stili Güzelleştirmek İçin (İsteğe bağlı: Yazı rengini siyah yapalım)
+            item->setForeground(Qt::black);
+
             ui->listVitrin->addItem(item);
         }
+    } else {
+        qDebug() << "Vitrin yükleme hatası:" << query.lastError().text();
     }
 }
 
