79158fb47a2014f80f8afdd1ab419beb210ade5a
Berat Belge <beratbelge03@gmail.com>
2026-05-13 00:36:23 +0300

    ilan_detai_ekrana_gelmesi
    ilan detau kısmında resim geliyor ama açıklama gelmşiyor

--- ilandetay.cpp
+++ ilandetay.cpp
@@ -1,80 +1,133 @@
 #include "ilandetay.h"
 #include "ui_ilandetay.h"
 #include "databasemanager.h"
+
 #include <QSqlQuery>
 #include <QSqlError>
 #include <QPixmap>
 #include <QDebug>
 #include <QMessageBox>
 
-IlanDetay::IlanDetay(int ilanNo, QWidget *parent) :
-    QWidget(parent), ui(new Ui::IlanDetay), m_ilanNo(ilanNo)
+IlanDetay::IlanDetay(int ilanNo, QWidget *parent)
+    : QWidget(parent),
+    ui(new Ui::IlanDetay),
+    m_ilanNo(ilanNo)
 {
     ui->setupUi(this);
+
     this->setStyleSheet("background-color: white;");
 
-    // Açılışta verileri yükle
     ilanBilgileriniYukle();
 }
 
-IlanDetay::~IlanDetay() {
+IlanDetay::~IlanDetay()
+{
     delete ui;
 }
 
-void IlanDetay::ilanBilgileriniYukle() {
-    QSqlDatabase db = DatabaseManager::getInstance()->getDatabase();
+void IlanDetay::ilanBilgileriniYukle()
+{
+    QSqlDatabase db =
+        DatabaseManager::getInstance()->getDatabase();
+
     QSqlQuery query(db);
 
-    // İlan ve Satıcı bilgilerini birleştirerek çekiyoruz
-    query.prepare("SELECT i.baslik, i.fiyat, i.durum, i.aciklama, i.fotografYolu, "
-                  "u.kullaniciAdi, u.email FROM Ilan i "
-                  "JOIN KULLANICI u ON i.saticiID = u.kullaniciID "
-                  "WHERE i.ilanNo = :id");
-    query.bindValue(":id", m_ilanNo);
+    query.prepare(
+        "SELECT baslik, fiyat, kategori, "
+        "aciklama, stokAdedi, fotografYolu "
+        "FROM Ilan "
+        "WHERE ilanNo = ?"
+        );
+
+    query.addBindValue(m_ilanNo);
 
     if (query.exec() && query.next()) {
-        // 1. BAŞLIK AYARI
-        ui->lblBaslik->setText(query.value("baslik").toString().toUpper());
 
-        // 2. FOTOĞRAF YÜKLEME
-        QString fotoYolu = query.value("fotografYolu").toString();
+        QString baslik =
+            query.value("baslik").toString();
+
+        QString fiyat =
+            query.value("fiyat").toString();
+
+        QString kategori =
+            query.value("kategori").toString();
+
+        QString aciklama =
+            query.value("aciklama").toString();
+
+        QString stok =
+            query.value("stokAdedi").toString();
+
+        QString fotoYolu =
+            query.value("fotografYolu").toString();
+
+        ui->lblBaslik->setText(
+            baslik.toUpper()
+            );
+
         if (!fotoYolu.isEmpty()) {
+
             QPixmap pix(fotoYolu);
-            if(!pix.isNull()) {
-                ui->lblFoto->setPixmap(pix.scaled(ui->lblFoto->size(), Qt::KeepAspectRatio, Qt::SmoothTransformation));
+
+            if (!pix.isNull()) {
+
+                ui->lblFoto->setPixmap(
+                    pix.scaled(
+                        ui->lblFoto->size(),
+                        Qt::KeepAspectRatio,
+                        Qt::SmoothTransformation
+                        )
+                    );
+
             } else {
-                ui->lblFoto->setText("Resim Bulunamadı");
+
+                ui->lblFoto->setText(
+                    "Resim Bulunamadi"
+                    );
             }
+
+        } else {
+
+            ui->lblFoto->setText(
+                "Fotograf Yok"
+                );
         }
 
-        // 3. DETAYLAR (Sadece başlıklar siyah ve kalın)
-        QString detayHtml = QString(
-            "<p><b style='color:black;'>İLAN:</b> %1</p>"
-            "<p><b style='color:black;'>FİYAT:</b> <span style='color:green;'>%2 TL</span></p>"
-            "<p><b style='color:black;'>DURUM:</b> %3</p>"
-        ).arg(query.value("baslik").toString()).arg(query.value("fiyat").toString()).arg(query.value("durum").toString());
-        ui->lblDetaylar->setText(detayHtml);
+        QString detay =
+            "<b>Ilan Basligi:</b> " + baslik +
+            "<br><br><b>Fiyat:</b> " + fiyat + " TL" +
+            "<br><br><b>Kategori:</b> " + kategori +
+            "<br><br><b>Stok Adedi:</b> " + stok;
 
-        // 4. SATICI BİLGİLERİ (Sadece başlıklar siyah ve kalın)
-        ui->lblSaticiBilgi->setText(QString("<b style='color:black;'>Satıcı:</b> %1<br><b style='color:black;'>E-Posta:</b> %2")
-                                    .arg(query.value("kullaniciAdi").toString())
-                                    .arg(query.value("email").toString()));
+        ui->lblDetaylar->setText(detay);
 
-        // 5. AÇIKLAMA
-        ui->txtAciklama->setText(query.value("aciklama").toString());
+        ui->txtAciklama->setText(aciklama);
+
         ui->txtAciklama->setReadOnly(true);
 
     } else {
-        qDebug() << "Veri çekme hatası:" << query.lastError().text();
+
+        qDebug() << "Veri cekme hatasi:"
+                 << query.lastError().text();
+
+        QMessageBox::critical(
+            this,
+            "Hata",
+            "Ilan bilgileri yuklenemedi!"
+            );
     }
 }
 
-// BUTON FONKSİYONLARI
-void IlanDetay::on_btnAnasayfa_clicked() {
-    this->close(); // Pencereyi kapatır ve Anasayfaya döner
+void IlanDetay::on_btnAnasayfa_clicked()
+{
+    this->close();
 }
 
-void IlanDetay::on_btnFavori_clicked() {
-    // Favori ekleme işlemi veritabanına burada kaydedilebilir
-    QMessageBox::information(this, "Başarılı", "İlan favorilerinize eklendi!");
+void IlanDetay::on_btnFavori_clicked()
+{
+    QMessageBox::information(
+        this,
+        "Basarili",
+        "Ilan favorilere eklendi!"
+        );
 }
--- ilanekle.cpp
+++ ilanekle.cpp
@@ -1,78 +1,134 @@
 #include "ilanekle.h"
 #include "ui_ilanekle.h"
 #include "databasemanager.h"
+
 #include <QFileDialog>
 #include <QMessageBox>
 #include <QSqlQuery>
 #include <QSqlError>
 #include <QDir>
 #include <QDateTime>
+#include <QFile>
+#include <QPixmap>
 
-IlanEkle::IlanEkle(QWidget *parent) : QWidget(parent), ui(new Ui::IlanEkle) {
+IlanEkle::IlanEkle(QWidget *parent)
+    : QWidget(parent),
+    ui(new Ui::IlanEkle)
+{
     ui->setupUi(this);
     secilenFotoYolu = "";
 }
 
-IlanEkle::~IlanEkle() {
+IlanEkle::~IlanEkle()
+{
     delete ui;
 }
 
-void IlanEkle::on_btnFotoEkle_clicked() {
-    QString dosya = QFileDialog::getOpenFileName(this, "Fotoğraf Seç", "", "Resimler (*.png *.jpg *.jpeg)");
+void IlanEkle::on_btnFotoEkle_clicked()
+{
+    QString dosya = QFileDialog::getOpenFileName(
+        this,
+        "Fotograf Sec",
+        "",
+        "Resimler (*.png *.jpg *.jpeg)"
+        );
+
     if (!dosya.isEmpty()) {
         secilenFotoYolu = dosya;
-        ui->lblFotoOnizleme->setPixmap(QPixmap(dosya).scaled(ui->lblFotoOnizleme->size(), Qt::KeepAspectRatio, Qt::SmoothTransformation));
+
+        ui->lblFotoOnizleme->setPixmap(
+            QPixmap(dosya).scaled(
+                ui->lblFotoOnizleme->size(),
+                Qt::KeepAspectRatio,
+                Qt::SmoothTransformation
+                )
+            );
     }
 }
 
-void IlanEkle::on_btnYayinla_clicked() {
+void IlanEkle::on_btnYayinla_clicked()
+{
     QString baslik = ui->txtBaslik->text();
     double fiyat = ui->txtFiyat->text().toDouble();
 
-    // Boşluk ve fiyat kontrolü
     if (baslik.isEmpty() || fiyat <= 0) {
-        QMessageBox::warning(this, "Hata", "Lütfen bir başlık ve geçerli bir fiyat giriniz!");
+        QMessageBox::warning(
+            this,
+            "Hata",
+            "Lutfen gecerli bilgiler giriniz!"
+            );
         return;
     }
 
-    // Veritabanı bağlantısını al
     QSqlDatabase db = DatabaseManager::getInstance()->getDatabase();
+
     if (!db.isOpen()) {
         if (!DatabaseManager::getInstance()->baglantiKur()) {
-            QMessageBox::critical(this, "Hata", "Veritabanı bağlantısı kurulamadı!");
+            QMessageBox::critical(
+                this,
+                "Hata",
+                "Veritabani baglantisi kurulamadi!"
+                );
             return;
         }
     }
 
-    // Fotoğraf kopyalama işlemleri
     QString kaydedilecekAdres = "";
+
     if (!secilenFotoYolu.isEmpty()) {
-        QString imagesDir = qApp->applicationDirPath() + "/images";
-        QDir().mkpath(imagesDir);
-        kaydedilecekAdres = imagesDir + "/" + QString::number(QDateTime::currentMSecsSinceEpoch()) + ".jpg";
+
+        QString imagesKlasoru =
+            qApp->applicationDirPath() + "/images";
+
+        QDir().mkpath(imagesKlasoru);
+
+        kaydedilecekAdres =
+            imagesKlasoru + "/" +
+            QString::number(QDateTime::currentMSecsSinceEpoch()) +
+            ".jpg";
+
         QFile::copy(secilenFotoYolu, kaydedilecekAdres);
     }
 
-    // Doğru bağlantı üzerinden sorguyu hazırla
     QSqlQuery query(db);
-    query.prepare("INSERT INTO Ilan (baslik, fiyat, kategori, aciklama, stokAdedi, fotografYolu) "
-                  "VALUES (:b, :f, :k, :a, :s, :foto)");
-    query.bindValue(":b", baslik);
-    query.bindValue(":f", fiyat);
-    query.bindValue(":k", ui->cmbKategori->currentText());
-    query.bindValue(":a", ui->txtAciklama->toPlainText());
-    query.bindValue(":s", 1);
-    query.bindValue(":foto", kaydedilecekAdres);
 
-    // Sorguyu çalıştır ve sonucu kontrol et
+    query.prepare(
+        "INSERT INTO Ilan "
+        "(baslik, fiyat, kategori, aciklama, stokAdedi, fotografYolu) "
+        "VALUES (?, ?, ?, ?, ?, ?)"
+        );
+
+    query.addBindValue(baslik);
+    query.addBindValue(fiyat);
+    query.addBindValue(ui->cmbKategori->currentText());
+    query.addBindValue(ui->txtAciklama->toPlainText());
+    query.addBindValue(1);
+    query.addBindValue(kaydedilecekAdres);
+
     if (query.exec()) {
-        QMessageBox::information(this, "Başarılı", "İlan başarıyla yayınlandı!");
-        this->close(); // Pencere kapanınca anasayfa tetiklenip vitrini güncelleyecek
+
+        QMessageBox::information(
+            this,
+            "Basarili",
+            "Ilan basariyla yayinlandi!"
+            );
+
+        this->close();
+
     } else {
-        QMessageBox::critical(this, "Sorgu Hatası", "İlan kaydedilemedi!\nHata: " + query.lastError().text());
+
+        QMessageBox::critical(
+            this,
+            "Sorgu Hatasi",
+            "Ilan kaydedilemedi!\nHata:\n" +
+                query.lastError().text()
+            );
+
+        qDebug() << query.lastError().text();
     }
 }
 
-void IlanEkle::on_btnIptal_clicked() {
+void IlanEkle::on_btnIptal_clicked()
+{
     this->close();
 }
